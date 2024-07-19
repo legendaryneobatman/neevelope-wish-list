13fce697530620b4267bd3017836eb1ef2eb5fde
Alexandr Novikov <legendaryneobatman@gmail.com>
2024-07-20 00:07:43 +0500

    trpc api test

--- server/trpc/trpc.ts
+++ server/trpc/trpc.ts
@@ -17,4 +17,4 @@ const t = initTRPC.context<Context>().create()
  **/
 export const publicProcedure = t.procedure;
 export const router = t.router;
-export const middleware = t.middleware:
+export const middleware = t.middleware;
